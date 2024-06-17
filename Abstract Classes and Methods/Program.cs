559ded7b98358a9a6a4e19932b0032efe85605ea
Frank Appiah <foappiah009@st.ug.edu.gh>
2024-06-17 10:27:00 +0000

    Abstract Classes and Methods

---
+++ Abstract Classes and Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿public abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double GetArea()
+    {
+        return length * width;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(7.0);
+        Rectangle rectangle = new Rectangle(7.0, 16.0);
+
+        Console.WriteLine("Circle area: " + circle.GetArea());
+        Console.WriteLine("Rectangle area: " + rectangle.GetArea());
+    }
+}
